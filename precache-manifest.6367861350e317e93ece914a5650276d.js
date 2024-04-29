cb217249fadcef999b30e8a9128e5d06325c3417
Marcin Kantarski <marcin.kantarski77@gmail.com>
2024-04-29 19:19:34 +0200

    Updates

---
+++ precache-manifest.6367861350e317e93ece914a5650276d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53df79bf28f1f06cf8ff21d7973907ae",
+    "url": "/currency-conventer-react/index.html"
+  },
+  {
+    "revision": "1f46d31cebdf31e5a06e",
+    "url": "/currency-conventer-react/static/js/2.7dbf82fe.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/currency-conventer-react/static/js/2.7dbf82fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e98797c35cef10cd5fc",
+    "url": "/currency-conventer-react/static/js/3.6ca5f8ce.chunk.js"
+  },
+  {
+    "revision": "f4959388fcec15c39eb2",
+    "url": "/currency-conventer-react/static/js/main.782fd535.chunk.js"
+  },
+  {
+    "revision": "4c34495c0674e38914a6",
+    "url": "/currency-conventer-react/static/js/runtime-main.e2b47936.js"
+  },
+  {
+    "revision": "78abde05e1f18bcd8d42e310f2a1d4f3",
+    "url": "/currency-conventer-react/static/media/background.78abde05.jpg"
+  }
+]);
