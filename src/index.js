266166a73e38eacd6f09b984c9fd68edc0520b9a
Marcin Kantarski <marcin.kantarski77@gmail.com>
2025-02-13 17:17:44 +0100

    change name of path for GlobalStyle

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GlobalStyle } from './globalStyles';
+import { GlobalStyle } from './GlobalStyle';
 import { theme } from './theme';
 import { ThemeProvider } from 'styled-components';
 
